1683b42014169805ee7baa7f54b2a375386a46d7
Kayla Raygoza <krraygoz@uci.edugit config --global user.name Kayla>
2024-11-13 21:59:00 -0800

    Complete Homework 3 with genome and annotation summaries to the best of my abilities. I hope it works!

--- code/scripts/hw3_annotation_summary.sh
+++ code/scripts/hw3_annotation_summary.sh
@@ -3,15 +3,22 @@
 # download
 wget https://ftp.flybase.net/releases/FB2022_05/dmel_r6.48/gtf/dmel-all-r6.48.gtf.gz
 
+if [ ! -f "dmel-all-r6.48.gtf.gz" ]; then
+  echo "GTF file not found!"
+  exit 1
+fi
+
 #check
 md5sum /data/homezvol2/krraygoz/myrepos/ee282/dmel-all-r6.48.gtf.gz
 
+gff_file="./dmel-all-r6.48.gtf.gz"
+
+
 # Count features by type using bioawk
 echo "Counting features by type:"
-bioawk -c gff '{print $3}' "$GTF_FILE" | sort | uniq -c | sort -nr
+bioawk -c gff '{print $3}' "$gff_file" | sort | uniq -c | sort -nr
 
 # Count genes per chromosome arm
 echo "Counting genes per chromosome arm:"
-bioawk -c gff '$3 == "gene" {print $1}' "$GTF_FILE" | \
+bioawk -c gff '$3 == "gene" {print $1}' "$gff_file" | \
   sed -E 's/([XY12L|R]{1}[0-9])/\1/' | sort | uniq -c
-
